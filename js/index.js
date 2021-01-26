5a8169f9b070e1be4cdd827b4aa35570907d9f47
Alejandro Dubon <alejandrodubon88@gmail.com>
2021-01-26 17:10:51 -0600

    finalizando el curso de programacion funcional con js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,81 @@
 const compose = (...functions) => data =>
     functions.reduceRight((value, func) => func(value), data);
 
+// const addAtributesToStr =  (obj = {}) => {
+//     const keys = Object.keys(obj);
+//     const attributes = [];
+
+//     for (let i = 0; i < keys.length; i++) {
+//         let attribute = keys[i];
+//         attributes.push(` ${attribute}="${obj[attribute]}"`);
+//     }
+//     return attributes.join('');
+// }
+
+const addAtributesToStr = (obj = {}) =>
+    Object.keys(obj)
+    .map((attribute) =>  ` ${attribute}="${obj[attribute]}"`)
+    .join('');
+
+
+const tagAttributes = obj => (content = '') => 
+`<${obj.tag}${obj.attributes ?  addAtributesToStr(obj.attributes) : ''}>${content}</${obj.tag}>`
+
+// const p = tagAttributes({
+//     tag: 'p',
+//     attributes: {
+//         class: 'post',
+//         color: 'red'
+//     }
+// });
+
+// const tag = t => {
+//     let addContent = '';
+//     if (typeof t === 'string') {
+//         addContent = tagAttributes({tag: t});
+
+//     } else {
+//         addContent = tagAttributes(t);
+//     }
+//     return addContent;
+// }
+
+const tag = t => typeof t === 'string' ? tagAttributes({tag: t}) : tagAttributes(t);    
+
+
+// Un clousure recuerda el contexto en el que fue creado
+// const generateHTMLTag = tag => content => `<${tag}>${content}</${tag}>`
+// const h1 = generateHTMLTag('h1');
+// h1('bla bla bla'); //<h1>bla bla bla</h1>
+
+
+//la funcion tag construye la funcion content
+// const h1 = tag('h1');
+// const p = tag({
+//     tag: 'p',
+//     attributes: {
+//         class: 'post',
+//         color: 'red'
+//     }
+// });
+
+
+const tr = tag('tr');
+// const trs = item => tr(tds(item));
+const trs = items => compose(tr, tds)(items)
+
+const td = tag('td');
+const tds = item => item.map(td).join('');
 
 const $DESCRIPTION = document.getElementById('$description');
 const $CALORIES = document.getElementById('$calories');
 const $CARBS = document.getElementById('$carbs');
 const $PROTEIN = document.getElementById('$protein');
 const $ADD = document.getElementById('$add');
+
+const $totalCalories = document.getElementById('total-calories');
+const $totalCarbs = document.getElementById('total-carbs');
+const $totalProtein = document.getElementById('total-protein');
 
 const $INPUTS = [$DESCRIPTION, $CALORIES, $CARBS, $PROTEIN];
 
@@ -34,13 +103,26 @@ const addToList = () => {
         protein: +$PROTEIN.value
     }
     list.push(newItem);
-    console.log(list);
 }
 
 const cleanInputs = () => {
     $INPUTS.forEach((input) => {
         input.value = '';
     })
+}
+
+const updateTotals = () => {
+    let calories = 0, carbs = 0, proteins = 0;
+
+    list.map(item => {
+        calories += item.calories;
+        carbs += item.carbs;
+        proteins += item.protein;
+    })
+
+    $totalCalories.innerHTML = `${calories}`;
+    $totalCarbs.innerHTML = `${carbs}`;
+    $totalProtein.innerHTML = `${proteins}`;
 }
 
 const validateInputs = () => {
@@ -51,9 +133,45 @@ const validateInputs = () => {
     if (inputsAreEmpty()){
         addToList();
         cleanInputs();
+        updateTotals();
+        renderItems();
     } else {
             console.log('Inputs are empty!');
     }
+}
+
+
+const renderItems = () => {
+    document.querySelector('tbody').innerHTML = ''
+
+    list.map((item, index) => {
+
+    const row = document.createElement('tr')
+    const removeButton = tag({
+        tag: 'button',
+        attributes: {
+            class: 'remove-button',
+            onclick: `removeItem(${index})`
+        }
+    })('eliminar')
+    row.innerHTML = trs([
+        item.description,
+        item.calories,
+        item.carbs,
+        item.protein,
+        removeButton
+    ]);
+
+    document.querySelector('tbody').appendChild(row)
+    });
+}
+
+
+const removeItem = (index) => {
+    list = list.filter((item, i) => i !== index);
+
+    updateTotals()
+    renderItems()
 }
 
 keyDownInput();
